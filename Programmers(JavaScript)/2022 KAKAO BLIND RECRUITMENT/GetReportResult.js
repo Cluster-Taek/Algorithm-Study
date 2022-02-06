45715cecd55ff994c3be8d24fe42581388bd9ff1
Cluster-Taek <yst811@naver.com>
2022-02-06 20:24:42 +0900

    신고 결과 받기

--- Programmers(JavaScript)/2022 KAKAO BLIND RECRUITMENT/GetReportResult.js
+++ Programmers(JavaScript)/2022 KAKAO BLIND RECRUITMENT/GetReportResult.js
@@ -10,8 +10,10 @@ function solution(id_list, report, k) {
         mailed_json[id] = 0;
     })
 
+    // 중복 제거
     const newReport = Array.from(new Set(report));
 
+    // 신고 횟수 json Setting
     newReport.map((row) => {
         const report_data = row.split(' ');
         if (json[report_data[1]] != null) {
@@ -19,13 +21,12 @@ function solution(id_list, report, k) {
         }
     })
 
-    id_list.map((id) => {
-        newReport.map((row) => {
-            const report_data = row.split(' ');
-            if (report_data[0] == id && json[report_data[1]] >= k) {
-                mailed_json[id] = mailed_json[id] + 1;
-            }
-        })
+    // 메일 횟수 json Setting
+    newReport.map((row) => {
+        const report_data = row.split(' ');
+        if (json[report_data[1]] >= k) {
+            mailed_json[report_data[0]] = mailed_json[report_data[0]] + 1;
+        }
     })
 
     id_list.map((id) => {
@@ -35,3 +36,25 @@ function solution(id_list, report, k) {
     return answer;
 }
 
+const testCase = [
+    {
+        "id_list" : ["muzi", "frodo", "apeach", "neo"],
+        "report" : ["muzi frodo","apeach frodo","frodo neo","muzi neo","apeach muzi"],
+        "k" : 2,
+        "result" : [2, 1, 1, 0]
+    },
+    {
+        "id_list" : ["con", "ryan"],
+        "report" : ["ryan con", "ryan con", "ryan con", "ryan con"],
+        "k" : 3,
+        "result" : [0,0]
+    }
+]
+
+testCase.map((test) => {
+    const result = solution(test.id_list, test.report, test.k);
+    console.log("-------------------------------------------")
+    console.log("입력값 )", test.id_list + " | " + test.report + " | " + test.k)
+    console.log("기댓값 )", test.result)
+    console.log("실행 결과 )", result)
+})
